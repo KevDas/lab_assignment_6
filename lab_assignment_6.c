12f6a760289dac2d6cdf2b3047b9fd5f45ec2f28
KevDas <ke763476@ucf.edu>
2023-06-24 21:13:20 -0400

    initial lab 6 commit

--- lab_assignment_6.c
+++ lab_assignment_6.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int search(int numbers[], int low, int high, int value) 
 {
-	return -1;
+	int mid;
+	if (low <= high)
+		{
+			mid = (low+high)/2;
+			if (value < numbers[mid])
+				return search(numbers, low, mid-1, value); 
+				
+			else if (value > numbers[mid])
+				return search(numbers, mid+1, high, value);
+
+			else
+				return mid + 1;
+		}
+return -1;
 }
 
 void printArray(int numbers[], int sz)
